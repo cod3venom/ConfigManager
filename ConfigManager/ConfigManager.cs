49fbcb2e9798df4adbd3f62e1e77fc82d21c1626
Levan <levan@skillsforge.pl>
2023-02-27 17:47:36 +0100

    initial commit

---
+++ ConfigManager/ConfigManager.cs
@@ -0,0 +1,56 @@
+﻿using ConfigManager.AbstractResource;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigManager
+{
+    public class ConfigManager
+    {
+        public readonly Storage _storage;
+        private FileInfo _configFile;
+        public ConfigManager(string configFilePath = "Settings.json") 
+        {
+            this.Initialize(configFilePath);
+            this._storage = new Storage(this._configFile);
+        }
+        
+        private void Initialize(string configFilePath)
+        {
+            if (!File.Exists(configFilePath))
+            {
+                File.WriteAllText(configFilePath, "{}");
+            }
+
+            this._configFile = new FileInfo(configFilePath);
+        }
+
+        public void AddField(string key, object value)
+        {
+            this._storage.Add(key, value);
+        }
+
+        public void RemoveField(string key)
+        {
+            this._storage.RemoveField(key);
+        }
+
+        public Dictionary<string, object> Read()
+        {
+            return this._storage.Read();
+        }
+
+        public void Clear()
+        {
+            this._storage.Purge();
+        }
+
+        public void Save()
+        {
+            this._storage.Save();
+        }
+    }
+}
---
+++ ConfigManager/Storage.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigManager.AbstractResource
+{
+    public class Storage : Dictionary<string, object>
+    {
+        private readonly FileInfo _configFile;
+
+        public Storage(FileInfo configFile)
+        {
+            this._configFile = configFile;
+        }
+
+        public void AddField(string key, object value)
+        {
+            if (this.ContainsKey(key)) {
+                this[key] = value; ;
+            } 
+            else {
+                this.Add(key, value);
+            }
+        }
+        public void RemoveField(string key)
+        {
+            if (!this.ContainsKey(key)) {
+                return;
+            }
+            this.Remove(key);
+        }
+
+        public Dictionary<string, object> Read()
+        {
+            string content = File.ReadAllText(this._configFile.FullName);
+            dynamic jsonData = JsonConvert.DeserializeObject<dynamic>(content);
+            foreach(KeyValuePair<string, object> kvp in jsonData) {
+                this.AddField(kvp.Key, kvp.Value);
+            }
+
+            return this;
+        }
+
+        public void Purge()
+        {
+            this.Clear();
+            this.Save();
+        }
+        public void Save()
+        {
+            string jsonContent = JsonConvert.SerializeObject(this);
+            File.WriteAllText(this._configFile.FullName, jsonContent);
+        }
+    }
+}
